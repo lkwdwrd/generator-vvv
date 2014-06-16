a448c3b8f66fff0242f0bede2efeb2f96a63c6b6
Luke Woodward <woodward.lucas@gmail.com>
2014-06-15 22:16:35 -0700

    import and plugin install fixes

--- app/templates/vvv-init.sh
+++ app/templates/vvv-init.sh
@@ -24,7 +24,12 @@ if [[ ! -z "$first_sql" ]] && [[ -f src/data/$first_sql ]]
 	if [[ -d htdocs ]]
 		then
 		cd htdocs
+		# Use the simpler single site setup for all find replace operations.
+		mv wp-config.php wp-config.php.old
+		echo "$(cat config/wp-constants)" | wp --allow-root core config --dbname="$database" --dbuser="$dbuser" --dbpass="$dbpass" --extra-php
 		wp --allow-root search-replace "$old_domain" "$new_domain" --skip-columns=guid
+		rm wp-config.php
+		mv wp-config.php.old wp-config.php
 		cd ../
 	else
 		sql_imported="yes"
@@ -47,7 +52,7 @@ if [[ ! -d htdocs ]]
 	wp --allow-root core download
 	echo "$constants" | wp --allow-root core config --dbname="$database" --dbuser="$dbuser" --dbpass="$dbpass" --extra-php
 	#Install as needed
-	if [[ ! $(wp --allow-root core is-installed) ]]
+	if ! $(wp --allow-root core is-installed)
 		then
 		wp --allow-root core install --url="$domain" --title="$site_name" --admin_user="$admin_user" --admin_password="$admin_pass" --admin_email="$admin_email"
 	fi
@@ -61,7 +66,19 @@ if [[ ! -d htdocs ]]
 		else
 			wp --allow-root core multisite-convert --title="$site_name"
 		fi
-	elif [[ "$multisite" == "yes" ]] && [[ "$sql_imported" == "yes" ]]
+	fi
+	# Update Database as Needed
+	if [[ "$sql_imported" == "yes" ]]
+		then
+		# Make sure we're using a single site config file to ensure search-replace works.
+		mv wp-config.php wp-config.php.old
+		echo "$(cat config/wp-constants)" | wp --allow-root core config --dbname="$database" --dbuser="$dbuser" --dbpass="$dbpass" --extra-php
+		wp --allow-root search-replace "$old_domain" "$new_domain" --skip-columns=guid
+		rm wp-config.php
+		mv wp-config.php.old wp-config.php
+	fi
+	# If this is multisite and we've imported SQL, update the DB.
+	if [[ "$multisite" == "yes" ]] && [[ "$sql_imported" == "yes" ]]
 		then
 		# Attempt to update the network sites if we importend it.
 		echo "Updating Network"
@@ -70,29 +87,29 @@ if [[ ! -d htdocs ]]
 		  wp --url="$url" --allow-root core update-db
 		done
 	fi
-	# Update Database as Needed
-	if [[ "$sql_imported" == "yes" ]]
-		then
-		wp --allow-root search-replace "$old_domain" "$new_domain" --skip-columns=guid
-	fi
 	# Move back to root to finish up shell commands.
 	cd ..
 fi
 
 #Install all WordPress.org plugins in the org_plugins file using CLI
-echo "Installing WordPress.org Plugins"
-if [[ -f ../config/org-plugins ]]
+echo "Checking for missing WordPress.org Plugins"
+if [[ config/org-plugins ]]
 then
+	# Move back into htdocs to install .org plugins.
+	cd htdocs
 	while IFS='' read -r line || [ -n "$line" ]
 	do
 		if [ "#" != "${line:0:1}" ]
 		then
-			if [[ ! $(wp --allow-root plugin is-installed "$line" ) ]]
+			# Only install the plugin if it's not already installed.
+			if ! $(wp --allow-root plugin is-installed $line )
 				then
-				wp plugin install "$line" --allow-root
+				wp plugin install $line --allow-root
 			fi
 		fi
 	done < ../config/org-plugins
+	# Move back to the root directory.
+	cd ..
 fi
 
 # Symlink working directories
@@ -101,13 +118,13 @@ find htdocs/wp-content/ -maxdepth 2 -type l -exec rm -f {} \;
 # Next attach symlinks for eacy of our types.
 # Plugins
 echo "Linking working directory pluins"
-find src/plugins/ -maxdepth 1 -mindepth 1 -exec ln -s $PWD/{} $PWD/htdocs/wp-content/plugins/ \;
+find src/plugins/ \( ! -regex '.*/\..*' \) -maxdepth 1 -mindepth 1 -exec ln -s $PWD/{} $PWD/htdocs/wp-content/plugins/ \;
 # Themes
 echo "Linking working directory themes"
-find src/themes/ -maxdepth 1 -mindepth 1 -exec ln -s $PWD/{} $PWD/htdocs/wp-content/themes/ \;
+find src/themes/ \( ! -regex '.*/\..*' \) -maxdepth 1 -mindepth 1 -exec ln -s $PWD/{} $PWD/htdocs/wp-content/themes/ \;
 # Dropins
 echo "Linking any available drop-ins"
-find src/dropins/ -maxdepth 1 -mindepth 1 -exec ln -s $PWD/{} $PWD/htdocs/wp-content/ \;
+find src/dropins/ \( ! -regex '.*/\..*' \) -maxdepth 1 -mindepth 1 -exec ln -s $PWD/{} $PWD/htdocs/wp-content/ \;
 
 # The Vagrant site setup script will restart Nginx for us
 echo "$site_name is now set up!";
