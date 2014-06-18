757d50d009341b35bd626d60bd93b16c892c7d85
Luke Woodward <woodward.lucas@gmail.com>
2014-06-18 14:30:30 -0700

    Adding untracked files

---
+++ app/templates/_Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function (grunt) {
+	'use strict';
+	// Project configuration
+	grunt.initConfig({
+		pkg:    grunt.file.readJSON('package.json'),
+		gitPull: {
+			themes: {
+				repos: [
+<%					var i, length;
+					for (i = 0, length = repos.theme.length; i < length; i++) {
+%>					{
+						path: ['src', 'themes'],
+						repo: '<%= repos.theme[i] %>'
+					}<% if((i + 1) !== length) { %>,<% } %>
+<% } %>				]
+			},
+			plugins: {
+				repos: [
+<%					for (i = 0, length = repos.plugin.length; i < length; i++) {
+%>					{
+						path: ['src', 'plugins'],
+						repo: '<%= repos.plugin[i] %>'
+					}<% if((i + 1) !== length) { %>,<% } %>
+<% } %>				]
+			}
+		},
+		vagrant_commands: {
+			restart: {
+				commands: [
+					['halt'],
+					['up', '--provision']
+				]
+			},
+		}
+	});
+
+	// Load tasks
+	require('load-grunt-tasks')(grunt);
+
+	// Register tasks
+	grunt.registerTask('default', ['gitPull', 'vagrant_commands:restart']);
+	grunt.registerTask('provision', ['vagrant_commands:restart']);
+
+	grunt.util.linefeed = '\n';
+};
