5925c2e55ab2d554d85817dc4ddc659031d7b33b
Luke Woodward <woodward.lucas@gmail.com>
2014-07-27 21:10:45 -0700

    Move main generator to create and give a discovery message instead

--- app/index.js
+++ app/index.js
@@ -1,56 +1,24 @@
 'use strict';
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
-var path = require('path');
-var prompts = require('../prompts/prompts.js');
-var output = require('../output/output.js');
 
 
 var VVVGenerator = yeoman.generators.Base.extend({
-  constructor: function () {
-    yeoman.generators.Base.apply(this, arguments);
-    var dirPath = '../templates';
-    this.sourceRoot(path.join(__dirname, dirPath));
-  },
-  init: function () {
-    this.pkg = require('../package.json');
-
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.installDependencies({
-          bower: false,
-          callback: function () {
-            if (!this.options['skip-grunt']) {
-              this.log(chalk.green.bold('Running default grunt task'));
-              this.spawnCommand('grunt', ['default']);
-            } else {
-              this.log(chalk.yellow('skipping grunt. Run grunt manually when you are ready.'));
-            }
-          }.bind(this)
-        });
-      }
-    });
-  },
-  welcome: function () {
-    // replace it with a short and sweet description of your generator
-    this.log(chalk.magenta('Thanks for generating auto site setups with yo vvv!'));
-  },
-  //prompts
-  getSiteInfo: prompts.getSiteInfo,
-  getWPInfo: prompts.getWPInfo,
-  promptSubdomains: prompts.promptSubdomains,
-  promptPlugins: prompts.promptPlugins,
-  haveRepos: prompts.haveRepos,
-  dependencyRepo: prompts.dependencyRepo,
-  // output
-  generateSiteId: output.generateSiteId,
-  vvv: output.vvv,
-  config: output.config,
-  src: output.src,
-  node: output.node,
-  scripts: output.scripts,
-  findSQL: output.findSQL,
-  sql: output.sql
+	notify: function () {
+		// replace it with a short and sweet description of your generator
+		this.log(chalk.magenta('Invoke a subgenerator to get started!'));
+		this.log('Available Modules:');
+		this.log('');
+		this.log(chalk.green.bold('\tyo vvv:json'));
+		this.log('\tCreate a vvv.json file.');
+		this.log('');
+		this.log(chalk.green.bold('\tyo vvv:bootstrap'));
+		this.log('\tRun a site setup from a vvv.json file.');
+		this.log('');
+		this.log(chalk.green.bold('\tyo vvv:create'));
+		this.log('\tCreate a site setup and run it.');
+		this.log('');
+	},
 });
 
 module.exports = VVVGenerator;
---
+++ create/index.js
@@ -0,0 +1,56 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var path = require('path');
+var prompts = require('../prompts/prompts.js');
+var output = require('../output/output.js');
+
+
+var VVVCreate = yeoman.generators.Base.extend({
+  constructor: function () {
+    yeoman.generators.Base.apply(this, arguments);
+    var dirPath = '../templates';
+    this.sourceRoot(path.join(__dirname, dirPath));
+  },
+  init: function () {
+    this.pkg = require('../package.json');
+
+    this.on('end', function () {
+      if (!this.options['skip-install']) {
+        this.installDependencies({
+          bower: false,
+          callback: function () {
+            if (!this.options['skip-grunt']) {
+              this.log(chalk.green.bold('Running default grunt task'));
+              this.spawnCommand('grunt', ['default']);
+            } else {
+              this.log(chalk.yellow('skipping grunt. Run grunt manually when you are ready.'));
+            }
+          }.bind(this)
+        });
+      }
+    });
+  },
+  welcome: function () {
+    // replace it with a short and sweet description of your generator
+    this.log(chalk.magenta('Thanks for generating auto site setups with yo vvv!'));
+  },
+  //prompts
+  getSiteInfo: prompts.getSiteInfo,
+  getWPInfo: prompts.getWPInfo,
+  promptSubdomains: prompts.promptSubdomains,
+  promptPlugins: prompts.promptPlugins,
+  haveRepos: prompts.haveRepos,
+  dependencyRepo: prompts.dependencyRepo,
+  // output
+  generateSiteId: output.generateSiteId,
+  vvv: output.vvv,
+  config: output.config,
+  src: output.src,
+  node: output.node,
+  scripts: output.scripts,
+  findSQL: output.findSQL,
+  sql: output.sql
+});
+
+module.exports = VVVCreate;
