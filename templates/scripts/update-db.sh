eb5f0a6b9e70dab4bb256d018a47e09b5c7eba0b
Luke Woodward <woodward.lucas@gmail.com>
2014-08-16 20:28:11 -0700

    add the network flag for multisite db replacements

--- templates/scripts/update-db.sh
+++ templates/scripts/update-db.sh
@@ -6,7 +6,7 @@ echo "Updating $site_name domains (this can take a while)."
 cd htdocs/
 if [[ "$multisite" == "yes" ]] && [[ ! -z $(wp --allow-root --url="$live_domain" option get siteurl) ]]
 	then
-	wp --allow-root --url="$live_domain" search-replace "$live_domain" "$domain" --skip-columns=guid
+	wp --allow-root --network --url="$live_domain" search-replace "$live_domain" "$domain" --skip-columns=guid
 else
 	wp --allow-root search-replace "$live_domain" "$domain" --skip-columns=guid
 fi
