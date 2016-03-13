14de777a6791a57986b65e7dbb1ce7a0a85def88
Luke Woodward <woodward.lucas@gmail.com>
2016-03-13 17:02:17 -0500

    First pass at advanced prompts for json subgenerator

--- generators/json/index.js
+++ generators/json/index.js
@@ -1,7 +1,85 @@
 'use strict';
-var yeoman = require('yeoman-generator');
+var Base = require( '../../lib/base' );
 
-module.exports = yeoman.Base.extend({
+module.exports = Base.extend({
 	description: 'another test',
-	funTime: function(){}
+	_compose: function() {
+		this.composeWith( 'vvv:source' );
+		this.composeWith( 'vvv:require' );
+		this.composeWith( 'vvv:dump', { arguments: [ 'manifest' ] } );
+	},
+	_initialize: function() {
+		this.removeRunMethod( 'selectInstall', 'initializing', 7 );
+		this.removeRunMethod( 'setUpAppPaths', 'initializing', 9 );
+		this.addRunMethod( 'initInstall', this._initInstall, 'initializing', 2 );
+		this.addRunMethod( 'advOptions', this._advancedOptions, 'prompting' );
+		this.addRunMethod( 'setupInstall', this._setupInstall, 'configuring' );
+		this.addRunMethod( 'setUpAppPaths', this.setUpAppPaths, 'configuring' );
+	},
+	_initInstall: function( done ) {
+		this.install = {
+			name: '',
+			title: '',
+			description: '',
+			homepage: '',
+			site: {},
+			server: {},
+			src: [],
+			composer: {
+				'type': 'root',
+				'keywords': [],
+				'minimum-stability': 'dev',
+				'prefer-stable': true,
+				'repositories': [{
+					'type': 'composer',
+					'url': 'https://wpackagist.org'
+				}],
+				'require': {
+					'vlucas/phpdotenv': '~2.2.0',
+					'johnpbloch/wordpress': '*'
+				}
+			}
+		};
+		done();
+	},
+	_setupInstall: function( done ) {
+		done();
+	},
+	_advancedOptions: function( done ) {
+		var prompts = this.getPrompt( 'advanced-options' );
+		this.prompt( prompts.questions, prompts.answers( this, done ) );
+	},
+	_advancedSite: function( done ) {
+		var prompts = this.getPrompt( 'advanced-site' );
+		prompts.questions[3].default = 'admin@' + this.install.server.local;
+		this.prompt( prompts.questions, prompts.answers( this, done ) );
+	},
+	_advancedConstants: function( done ) {
+		var prompts = this.getPrompt( 'advanced-constants' );
+		this.prompt( prompts.questions, prompts.answers( this, done ) );
+	},
+	_advancedEnv: function( done ) {
+		var prompts = this.getPrompt( 'advanced-env' );
+		this.prompt( prompts.questions, prompts.answers( this, done ) );
+	},
+	_advancedProxies: function( done ) {
+		var prompts = this.getPrompt( 'advanced-proxies' );
+		if ( this.install.server.remote ) {
+			prompts.internalQuestions[1].default = 'https://' + this.install.server.remote;
+		}
+		this.prompt( prompts.questions, prompts.answers( this, done ) );
+	},
+	_advancedPaths: function( done ) {
+		var prompts = this.getPrompt( 'advanced-paths' );
+		this.prompt( prompts.questions, prompts.answers( this, done ) );
+	},
+	_advancedComposer: function( done ) {
+		var prompts = this.getPrompt( 'advanced-composer' );
+		this.prompt( prompts.questions, prompts.answers( this, done ) );
+	},
+	prompting: function() {
+		var done = this.async(),
+		prompts = this.getPrompt( 'basic-manifest' );
+		this.prompt( prompts.questions, prompts.answers( this, done ) );
+	}
 });
--- generators/source/index.js
+++ generators/source/index.js
@@ -1,5 +1,4 @@
 var Base = require( '../../lib/base' );
-var inquirer = require( 'inquirer' );
 var chalk = require( 'chalk' );
 
 module.exports = Base.extend({
@@ -9,139 +8,9 @@ module.exports = Base.extend({
 	},
 	prompting: function() {
 		var done = this.async(),
-			prompts = [
-				{
-					type: 'list',
-					name: 'type',
-					message: 'This source is a:',
-					choices: [
-						{
-							name: 'Git Repository',
-							value: 'git'
-						},
-						{
-							name: 'Subversion Repository',
-							value: 'svn'
-						},
-						new inquirer.Separator(),
-						{
-							name: 'No more, I\'m done',
-							value: 'done'
-						}
-					]
-				},
-				{
-					when: makeWhen( 'done', 'type', true ),
-					type: 'list',
-					name: 'map',
-					message: 'This repository contains a:',
-					choices: [
-						{
-							name: 'Site Root',
-							value: 'root'
-						},
-						{
-							name: 'Content Folder',
-							value: 'content'
-						},
-						{
-							name: 'WP Plugin',
-							value: 'plugin'
-						},
-						{
-							name: 'WP Theme',
-							value: 'theme'
-						},
-						{
-							name: 'WP MU Plugin',
-							value: 'muplugin'
-						},
-						{
-							name: 'A Dropin Plugin',
-							value: 'dropin'
-						},
-						{
-							name: 'VIP Resource',
-							value: 'vip'
-						},
-						{
-							name: 'Plugins Folder',
-							value: 'plugins-folder'
-						},
-						{
-							name: 'Themes Folder',
-							value: 'themes-folder'
-						},
-						{
-							name: 'MU Plugins Folder',
-							value: 'mu-plugins-folder'
-						}
-					]
-				},
-				{
-					when: makeWhen( 'done', 'type', true ),
-					name: 'url',
-					message: 'The repository URL is:'
-				},
-				{
-					when: makeWhen( [ 'theme', 'plugin', 'muplugin' ], 'map' ),
-					filter: filterName,
-					name: 'name',
-					message: 'The name in the composer.json file is (blank if none):'
-				},
-				{
-					when: makeWhen( undefined, 'name', true ),
-					name: 'stable',
-					message: 'The stable version of this source is:'
-				},
-				{
-					when: makeWhen( 'done', 'type', true ),
-					type: 'confirm',
-					name: 'confirmed',
-					message: 'Alright, I got it. Is everything correct?'
-				}
-			];
-
-		function processAnswers( answers ) {
-			if ( 'done' === answers.type ) {
-				return done();
-			}
-
-			if ( ! answers.confirmed ) {
-				this.log( chalk.red.bold( 'OK, let\'s try again.' ) );
-				this.prompt( prompts, processAnswers.bind( this ) );
-			} else {
-				delete answers.confirmed;
-			}
-
-			if ( !answers.name ) {
-				delete answers.name;
-			}
-
-			this.install.src.push( answers );
-
-			this.log( chalk.green.bold( 'All right, your source has been noted!' ) );
-			this.log( chalk.magenta( 'Want to add another?' ) );
-			this.prompt( prompts, processAnswers.bind( this ) );
-		}
-		function filterName( val ) {
-			return ( val ) ? val : undefined;
-		}
-		function makeWhen( test, key, present ) {
-			if ( 'object' !== typeof test || ! test instanceof Array ) {
-				test = [ test ];
-			}
-			present = !present;
-			return function _when( answers ) {
-				if ( present ) {
-					return -1 !== test.indexOf( answers[ key ] );
-				} else {
-					return -1 === test.indexOf( answers[ key ] );
-				}
-			};
-		}
+			prompts = this.getPrompt( 'source' );
 
 		this.log( chalk.magenta.bold( 'Let\'s add some sources!' ) );
-		this.prompt( prompts, processAnswers.bind( this ) );
+		this.prompt( prompts.questions, prompts.answers( this, done ) );
 	}
 });
